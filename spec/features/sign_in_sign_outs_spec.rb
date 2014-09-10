12bf2111346a75f3f87d94f05151fc574a969b79
Daniel Mortensen <daniel.j.mortensen@gmail.com>
2014-09-10 20:13:28 +0100

    sign_in_sign_out test pass

--- spec/features/sign_in_sign_outs_spec.rb
+++ spec/features/sign_in_sign_outs_spec.rb
@@ -9,15 +9,16 @@ feature 'sign in sign out' do
   scenario 'successfully' do
     user = User.new(:email => 'j@g.com', :password => 'password') 
     user.confirmed_at = Time.now
+    user.save
     
     visit root_path
-    save_and_open_page
     click_link 'Login'
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
+    click_button 'Sign in'
 
-    expect(page).to have_content('Successfully logged in.')
-    
+    expect(page).to have_content('Signed in successfully')
     
   end
+
 end
